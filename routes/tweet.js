16db3e27293acde47c66601c8edb0a3edcfd164a
Nassim <nassim.hiber@gmail.com>
2025-03-27 12:05:26 +0100

    feat: model et route tweet init

---
+++ routes/tweet.js
@@ -0,0 +1,15 @@
+require('../models/connection');
+
+const express = require('express');
+const router = express.Router();
+const moment = require("moment");
+
+const Tweet = require('../models/tweet');
+
+
+//Route post
+
+//route get
+
+
+module.exports = router;
