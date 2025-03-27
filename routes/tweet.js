9fae38aee1b936c69489ae3a8d2c079ca3fc67b5
GGhezali <gregory.ghezali@hotmail.fr>
2025-03-27 14:20:39 +0100

    POST add and remove user in usersLike list

--- routes/tweet.js
+++ routes/tweet.js
@@ -23,6 +23,32 @@ router.get("/", (req, res) => {
     });
   });
   
-  //----------------------------------------------------------------------------
+  
+//----------------------------------------------------------------------------
+
+/* POST add user in usersLike list */
+router.post("/addUserLike", (req, res) => {
+    const { username, content } = req.body;
+  Tweet.updateOne(
+    {content : content},
+    { $push: { usersLike: username } }
+    ).then((data) => {
+        res.json({ result: true });
+});
+
+//----------------------------------------------------------------------------
+
+/* POST remove user in usersLike list */
+router.post("/deleteUserLike", (req, res) => {
+    const { username, content } = req.body;
+  Tweet.updateOne(
+    {content : content},
+    { $pull: { usersLike: username } }
+    ).then((data) => {
+        res.json({ result: true });
+});
+
+//----------------------------------------------------------------------------//----------------------------------------------------------------------------
+
 
 module.exports = router;
