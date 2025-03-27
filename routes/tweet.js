01b935457b9bbefb5e534b31283abf63f7db0fec
Nassim <nassim.hiber@gmail.com>
2025-03-27 14:49:37 +0100

    feat: route post

--- routes/tweet.js
+++ routes/tweet.js
@@ -1,28 +1,37 @@
-require('../models/connection');
+require("../models/connection");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const moment = require("moment");
-const Tweet = require('../models/tweet');
-
+const Tweet = require("../models/tweet");
 
 //Route post
-router.post('/add', (req, res) => {
-    console.log("post ok")
-    res.json({result: true})
-})
+router.post("/", (req, res) => {
+  //Create new tweet
+  const newTweet = new Tweet({
+    firstname: req.body.firstname,
+    username: req.body.username,
+    time: req.body.time,
+    content: req.body.content,
+    usersLike: req.body.usersLike,
+    hashtagList: req.body.hashtagList,
+  });
+  newTweet.save().then((newDoc) => {
+    res.json({ result: true, newDoc });
+  });
+});
 
 //route get
 //----------------------------------------------------------------------------
 
 /* GET tweet list */
 router.get("/", (req, res) => {
-    Tweet.find().then((data) => {
-      res.json({ data });
-    });
+  Tweet.find().then((data) => {
+    res.json({ data });
   });
-  
-  //----------------------------------------------------------------------------
+});
+
+//----------------------------------------------------------------------------
 
 module.exports = router;
